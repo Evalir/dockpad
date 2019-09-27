6786ba45d3724183479619929e6b3b34f54d237a
Albin Frias <albinesteban@outlook.com>
2019-09-27 00:44:33 -0400

    Added config and NavigationConstants class

---
+++ Dockpad/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dockpad
+{
+    class Config
+    {
+        public const string DomainURL = "https://dockpad.xyz/";
+    }
+}
