c9db0bfc22a4d8d7b360dda93f43d44fa970059d
Enrique Ortiz <enrique@cuyaya.com>
2019-10-02 19:51:04 -0400

    Add navigation constants, document response class

--- Dockpad/Helpers/Response.cs
+++ Dockpad/Helpers/Response.cs
@@ -4,6 +4,12 @@ using System.Text;
 
 namespace Dockpad.Helpers
 {
+    /// <summary>
+    /// The Response Class
+    /// Used to handle API responses clearly, receiving the data and handling
+    /// errors dpeending on the status code.
+    /// </summary>
+    /// <typeparam name="T">Expected API Model</typeparam>
     public class Response<T>
     {
         public T Data;
