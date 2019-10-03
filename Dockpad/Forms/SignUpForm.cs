52db34d6f9a56cbfaa0ab386ec4124cde2808fbc
Roniel Valdez <roniel_valdez@outlook.com>
2019-10-02 23:09:54 -0400

    Fixed register issue

--- Dockpad/Forms/SignUpForm.cs
+++ Dockpad/Forms/SignUpForm.cs
@@ -10,8 +10,6 @@ namespace Dockpad.Forms
         public string lastName { get; set; }
         public string email { get; set; }
         public string username { get; set; }
-        //  TODO: Remove required field for birth date (will be done on backend)
-        public string birthDate { get; set; } = "2019-01-01";
         public string phoneNumber { get; set; }
         public string password { get; set; }
         public string passwordConfirmation { get; set; }
