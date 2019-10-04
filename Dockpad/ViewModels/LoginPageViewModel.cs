2e76fda18d8432e713e26d2dc5f2c2aa30b9d985
Albin Frias <albinesteban@outlook.com>
2019-10-04 01:20:55 -0400

    Added profile view with the API data

--- Dockpad/ViewModels/LoginPageViewModel.cs
+++ Dockpad/ViewModels/LoginPageViewModel.cs
@@ -1,18 +1,11 @@
 ﻿using Dockpad.Forms;
 using Dockpad.Models;
 using Prism.Commands;
-using Prism.Mvvm;
 using Prism.Navigation;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Text;
-using Xamarin.Forms;
 using Dockpad.Services;
-using System.Threading.Tasks;
 using Dockpad.Helpers;
-using System.Diagnostics;
-using Dockpad.Views;
 
 namespace Dockpad.ViewModels
 {
@@ -52,7 +45,7 @@ namespace Dockpad.ViewModels
             {
                 // TODO: Resolve where the user data will be handled or saved
                 Config.Token = $"Token {response.Data.Token}";
-                await _navigationService.NavigateAsync(new Uri("RegisterPage", UriKind.Relative));
+                await _navigationService.NavigateAsync(new Uri("HomePage", UriKind.Relative));
             } else if (response.ErrorData.StatusCode == System.Net.HttpStatusCode.BadRequest)
             {
                 // Wrong username/password
--- Dockpad/ViewModels/UserPageViewModel.cs
+++ Dockpad/ViewModels/UserPageViewModel.cs
@@ -1,10 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Dockpad.Models;
+using Prism.Navigation;
+using Dockpad.Services;
+using Dockpad.Helpers;
+using System.ComponentModel;
+using Prism.Mvvm;
 
 namespace Dockpad.ViewModels
 {
-    class UserPageViewModel
+    public class UserPageViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        INavigationService _navigationService;
+        PRMAPIService API { get; set; }
+        public User Profile { get; set; } = new User();
+        
+        public UserPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            API = new PRMAPIService();
+            LoadProfile();
+        }
+
+        public async void LoadProfile()
+        {
+            Response<User> response = await API.GetProfile();
+            if (response.ErrorData == null)
+            {
+                Profile = response.Data;
+                
+            }
+            else if (response.ErrorData.StatusCode == System.Net.HttpStatusCode.BadRequest)
+            {
+                Profile.FirstName = "Error getting your profile data";
+            }
+        }
     }
 }
