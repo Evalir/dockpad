bac2c21c72545fa5b0de834ae06d7280896c83b9
Albin Frias <albinesteban@outlook.com>
2019-10-07 23:53:13 -0400

    Added recent events tab to user profile

--- Dockpad/ViewModels/UserPageViewModel.cs
+++ Dockpad/ViewModels/UserPageViewModel.cs
@@ -4,6 +4,7 @@ using Dockpad.Services;
 using Dockpad.Helpers;
 using System.ComponentModel;
 using Prism.Mvvm;
+using System.Collections.Generic;
 
 namespace Dockpad.ViewModels
 {
@@ -11,24 +12,42 @@ namespace Dockpad.ViewModels
     {
         public User User { get; set; } = new User();
         public Profile Profile { get; set; } = new Profile();
+
+        public List<Event> Events { get; set; }
         public UserPageViewModel(INavigationService navigationService) : base(navigationService)
         {
             API = new PRMAPIService();
             LoadProfile();
+            LoadEvents();
         }
 
-        public async void LoadProfile()
+        private async void LoadProfile()
         {
+
             Response<User> response = await API.GetProfile();
             if (response.ErrorData == null)
             {
-               User  = response.Data;
-               Profile = User.Profile;
+                User = response.Data;
+                Profile = User.Profile;
             }
             else if (response.ErrorData.StatusCode == System.Net.HttpStatusCode.BadRequest)
             {
                 User.FirstName = "Error getting your profile data";
             }
         }
+
+        private async void LoadEvents()
+        {
+            Response<PaginatedResponse<Event>> response = await API.GetAllEvents();
+            if(response.ErrorData == null)
+            {
+                Events = new List<Event>(response.Data.Results);
+            }
+            else if(response.ErrorData.StatusCode == System.Net.HttpStatusCode.BadRequest)
+            {
+                //Handle error here
+            }            
+        }
+
     }
 }
