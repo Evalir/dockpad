362e5c2c3e5a9a005c5082df37c818da56369583
Roniel Valdez <roniel_valdez@outlook.com>
2019-10-08 21:46:03 -0400

    Implemented several endpoints

--- Dockpad/Services/APIManager.cs
+++ Dockpad/Services/APIManager.cs
@@ -1,12 +1,11 @@
 ﻿using Dockpad.Forms;
+using Dockpad.Models;
 using Fusillade;
 using Polly;
 using Refit;
 using System;
-using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Dockpad.Services
@@ -44,7 +43,7 @@ namespace Dockpad.Services
             return data;
         }
 
-        public async Task<HttpResponseMessage> Login([Body] LoginForm data)
+        public async Task<HttpResponseMessage> Login(LoginForm data)
         {
             return await RemoteRequestAsync<HttpResponseMessage>(prmApi.GetApi(Priority.UserInitiated).Login(data));
         }
@@ -54,14 +53,97 @@ namespace Dockpad.Services
             return await RemoteRequestAsync<HttpResponseMessage>(prmApi.GetApi(Priority.UserInitiated).GetProfile(token));
         }
 
-        public async Task<HttpResponseMessage> SignUp([Body] SignUpForm data)
+        public async Task<HttpResponseMessage> SignUp(SignUpForm data)
         {
             return await RemoteRequestAsync<HttpResponseMessage>(prmApi.GetApi(Priority.UserInitiated).SignUp(data));
         }
 
-        public async Task<HttpResponseMessage> GetAllEvents(string token)
+        public async Task<HttpResponseMessage> GetEvents(string token)
         {
-            return await RemoteRequestAsync<HttpResponseMessage>(prmApi.GetApi(Priority.UserInitiated).GetAllEvents(token));
+            return await RemoteRequestAsync<HttpResponseMessage>(prmApi.GetApi(Priority.UserInitiated).GetEvents(token));
+        }
+
+        public async Task<HttpResponseMessage> GetActivities(string token)
+        {
+            return await RemoteRequestAsync<HttpResponseMessage>(prmApi.GetApi(Priority.UserInitiated).GetActivities(token));
+        }
+
+        public async Task<HttpResponseMessage> GetMoods(string token)
+        {
+            return await RemoteRequestAsync<HttpResponseMessage>(prmApi.GetApi(Priority.UserInitiated).GetMoods(token));
+        }
+
+        public async Task<HttpResponseMessage> GetMoodsInterval(string token, string fromDate, string toDate)
+        {
+            return await RemoteRequestAsync<HttpResponseMessage>(prmApi.GetApi(Priority.UserInitiated)
+                .GetMoodsInterval(token, fromDate, toDate));
+        }
+
+        public async Task<HttpResponseMessage> GetEventsInterval(string token, string fromDate, string toDate)
+        {
+            return await RemoteRequestAsync<HttpResponseMessage>(prmApi.GetApi(Priority.UserInitiated)
+                .GetEventsInterval(token, fromDate, toDate));
+        }
+
+        public async Task<HttpResponseMessage> GetEventDetail(string token, string code)
+        {
+            return await RemoteRequestAsync<HttpResponseMessage>(prmApi.GetApi(Priority.UserInitiated)
+                .GetEventDetail(token, code));
+        }
+
+        public async Task<HttpResponseMessage> PostActivity(string token, Activity activity)
+        {
+           return await RemoteRequestAsync<HttpResponseMessage>(prmApi.GetApi(Priority.UserInitiated)
+               .PostActivity(token, activity));
+        
+        }
+
+        public async Task<HttpResponseMessage> PutActivity(string token, Activity activity, string code)
+        {
+            return await RemoteRequestAsync<HttpResponseMessage>(prmApi.GetApi(Priority.UserInitiated)
+                .PutActivity(token, activity, code));
+        }
+
+        public async Task<HttpResponseMessage> PatchActivity(string token, Activity activity, string code)
+        {
+            return await RemoteRequestAsync<HttpResponseMessage>(prmApi.GetApi(Priority.UserInitiated)
+                .PatchActivity(token, activity, code));
+        }
+
+        public async Task<HttpResponseMessage> GetActivitiesLogs([Header("Authorization")] string token)
+        {
+            return await RemoteRequestAsync<HttpResponseMessage>(prmApi.GetApi(Priority.UserInitiated)
+                .GetActivitiesLogs(token));
+        }
+
+        public async Task<HttpResponseMessage> GetActivitiesLogs([Header("Authorization")] string token, string activityCode)
+        {
+            return await RemoteRequestAsync<HttpResponseMessage>(prmApi.GetApi(Priority.UserInitiated)
+                .GetActivitiesLogs(token, activityCode));
+        }
+
+        public async Task<HttpResponseMessage> GetActivityLogDetail([Header("Authorization")] string token, string activityCode, string logCode)
+        {
+            return await RemoteRequestAsync<HttpResponseMessage>(prmApi.GetApi(Priority.UserInitiated)
+                .GetActivityLogDetail(token, activityCode, logCode));
+        }
+
+        public async Task<HttpResponseMessage> PostActivityLog([Header("Authorization")] string token, string activityCode, ActivityLog activityLog)
+        {
+            return await RemoteRequestAsync<HttpResponseMessage>(prmApi.GetApi(Priority.UserInitiated)
+                .PostActivityLog(token, activityCode, activityLog));
+        }
+
+        public async Task<HttpResponseMessage> PutActivityLog([Header("Authorization")] string token, string activityCode, ActivityLog activityLog, string logCode)
+        {
+            return await RemoteRequestAsync<HttpResponseMessage>(prmApi.GetApi(Priority.UserInitiated)
+                .PutActivityLog(token, activityCode, activityLog, logCode));
+        }
+
+        public async Task<HttpResponseMessage> PatchActivityLog([Header("Authorization")] string token, string activityCode, ActivityLog activityLog, string logCode)
+        {
+            return await RemoteRequestAsync<HttpResponseMessage>(prmApi.GetApi(Priority.UserInitiated)
+                .PatchActivityLog(token, activityCode, activityLog, logCode));
         }
     }
 }
--- Dockpad/Services/IPRMAPI.cs
+++ Dockpad/Services/IPRMAPI.cs
@@ -12,6 +12,7 @@ namespace Dockpad.Services
 {
     public interface IPRMAPI
     {
+        #region User
         [Post("/users/signup/")]
         Task<HttpResponseMessage> SignUp([Body] SignUpForm data);
 
@@ -20,8 +21,70 @@ namespace Dockpad.Services
 
         [Get("/users/profile/")]
         Task<HttpResponseMessage> GetProfile([Header("Authorization")] string token);
+        #endregion
 
+        #region Events
         [Get("/events/")]
-        Task<HttpResponseMessage> GetAllEvents([Header("Authorization")] string token);
+        Task<HttpResponseMessage> GetEvents([Header("Authorization")] string token);
+        
+        [Get("/events/")]
+        Task<HttpResponseMessage> GetEventsInterval([Header("Authorization")] string token,
+            [AliasAs("from")] string fromDate, [AliasAs("to")] string toDate);
+
+        [Get("/events/{code}")]
+        Task<HttpResponseMessage> GetEventDetail([Header("Authorization")] string token, string code);
+
+       // [Post("/events/")]
+       // Task<HttpResponseMessage> PostEvent([Header("Authorization")] string token, Event newEvent);
+        #endregion
+
+        #region Activities
+        [Get("/activities/")]
+        Task<HttpResponseMessage> GetActivities([Header("Authorization")] string token);
+
+        [Get("/activities/{code}")]
+        Task<HttpResponseMessage> GetActivityDetail([Header("Authorization")] string token, string code);
+
+        [Post("/activities/")]
+        Task<HttpResponseMessage> PostActivity([Header("Authorization")] string token, [Body] Activity activity);
+
+        [Put("/activities/{code}")]
+        Task<HttpResponseMessage> PutActivity([Header("Authorization")] string token, [Body] Activity activity, string code);
+
+        [Patch("/activities/{code}")]
+        Task<HttpResponseMessage> PatchActivity([Header("Authorization")] string token, [Body] Activity activity, string code);
+        #endregion
+
+        #region Activity Logs
+
+        [Get("/activities/logs/")]
+        Task<HttpResponseMessage> GetActivitiesLogs([Header("Authorization")] string token);
+
+        [Get("/activities/{activityCode}/logs/")]
+        Task<HttpResponseMessage> GetActivitiesLogs([Header("Authorization")] string token, string activityCode);
+
+        [Get("/activities/{activityCode}/logs/{logCode}")]
+        Task<HttpResponseMessage> GetActivityLogDetail([Header("Authorization")] string token, string activityCode, string logCode);
+
+        [Post("/activities/{activityCode}/logs/")]
+        Task<HttpResponseMessage> PostActivityLog([Header("Authorization")] string token, string activityCode, [Body] ActivityLog activityLog);
+
+
+        [Put("/activities/{activityCode}/logs/{logCode}")]
+        Task<HttpResponseMessage> PutActivityLog([Header("Authorization")] string token, string activityCode, [Body] ActivityLog activityLog, string logCode);
+
+        [Patch("/activities/{activityCode}/logs/{logCode}")]
+        Task<HttpResponseMessage> PatchActivityLog([Header("Authorization")] string token, string activityCode, [Body] ActivityLog activityLog, string logCode);
+
+        #endregion
+
+        #region Moods
+        [Get("/moods/")]
+        Task<HttpResponseMessage> GetMoods([Header("Authorization")] string token);
+
+        [Get("/moods/")]
+        Task<HttpResponseMessage> GetMoodsInterval([Header("Authorization")] string token, 
+            [AliasAs("from")] string fromDate, [AliasAs("to")] string toDate);
+        #endregion
     }
 }
