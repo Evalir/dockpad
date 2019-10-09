b1eb5e0c05cc2575391f330b4e81b0f7ea48d66e
Enrique Ortiz <enrique@cuyaya.com>
2019-10-08 23:03:48 -0400

    Finish mood log implementation

--- Dockpad/ViewModels/AddMoodPageViewModel.cs
+++ Dockpad/ViewModels/AddMoodPageViewModel.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using System.ComponentModel;
+using Prism.Commands;
 using Prism.Navigation;
 
 namespace Dockpad.ViewModels
 {
     public class AddMoodPageViewModel : BaseViewModel
     {
+
+        public string SelectedMood { get; set; }
+        public DelegateCommand<object> SelectMoodCommand { get; set; }
+        public DelegateCommand SaveMoodCommand { get; set; }
+
         public AddMoodPageViewModel(INavigationService navigationService) : base(navigationService)
         {
+            SelectMoodCommand = new DelegateCommand<object>(SelectMood);
+        }
+
+        private void SelectMood(object Mood)
+        {
+            var moodType = Convert.ToString(Mood);
+            SelectedMood = moodType;
         }
     }
 }
