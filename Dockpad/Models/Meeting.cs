74cc65c4506356837d2a4b40fa810c0a80401d4b
Albin Frias <albinesteban@outlook.com>
2019-10-08 21:38:04 -0400

    Added JsonProperty to all models

--- Dockpad/Models/Meeting.cs
+++ Dockpad/Models/Meeting.cs
@@ -2,18 +2,34 @@
 using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
+using Newtonsoft.Json;
 
 namespace Dockpad.Models
 {
     public class Meeting
     {
+        [JsonProperty("eventName")]
         public string EventName { get; set; }
+
+        [JsonProperty("organizer")]
         public string Organizer { get; set; }
+
+        [JsonProperty("contactID")]
         public string ContactID { get; set; }
+
+        [JsonProperty("capacity")]
         public int Capacity { get; set; }
+
+        [JsonProperty("from")]
         public DateTime From { get; set; }
+
+        [JsonProperty("to")]
         public DateTime To { get; set; }
+
+        [JsonProperty("color")]
         public Color color { get; set; }
+
+        [JsonProperty("allDay")]
         public bool AllDay { get; set; }
     }
 }
--- Dockpad/Models/Mood.cs
+++ Dockpad/Models/Mood.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace Dockpad.Models
 {
     public class Mood : INotifyPropertyChanged
     {
+
+        [JsonProperty("owner")]
         public string Owner { get; set; }
 
+        [JsonProperty("mood")]
         public string mood { get; set; }
 
+        [JsonProperty("description")]
         public string Description { get; set; }
 
+        [JsonProperty("date")]
         public DateTime Date { get; set; }
 
         public Mood()
