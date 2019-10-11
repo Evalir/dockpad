b74fcd19819be1e917ab9879adde133b33d5155e
Albin Frias <albinesteban@outlook.com>
2019-10-11 00:49:04 -0400

    Finished activities view

--- Dockpad/ViewModels/ActivityPageViewModel.cs
+++ Dockpad/ViewModels/ActivityPageViewModel.cs
@@ -1,10 +1,101 @@
-﻿using System;
+﻿using Dockpad.Helpers;
+using Dockpad.Models;
+using Dockpad.Services;
+using Newtonsoft.Json;
+using Prism.Commands;
+using Prism.Navigation;
+using Prism.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Dockpad.ViewModels
 {
-    class ActivityPageViewModel
+    public class ActivityPageViewModel : BaseViewModel
     {
+
+        public ObservableCollection<Activity> Activities { get; set; } = new ObservableCollection<Activity>();
+
+        private Activity _selectedActivity;
+
+        private IAPIManager _apiManager;
+
+        public DelegateCommand AddActivityCommand { get; set; }
+
+        private IPageDialogService _pageDialog;
+        public Activity SelectedActivity
+        {
+            get => _selectedActivity;
+            set
+            {
+                _selectedActivity = value;
+                if (_selectedActivity == null)
+                    return;
+                ExecuteSelectActivity(_selectedActivity);
+                SelectedActivity = null;
+            }
+        }
+        public ActivityPageViewModel(INavigationService navigationService, IAPIManager apiManager, IPageDialogService pageDialog) : base(navigationService)
+        {
+            _apiManager = apiManager;
+            _pageDialog = pageDialog;
+            AddActivityCommand = new DelegateCommand(ExecuteAddActivity);
+            LoadActivities();
+        }
+
+        private async void LoadActivities()
+        {
+            var response = await _apiManager.GetActivities(Config.Token);
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                PaginatedResponse<Activity> activities = await Task.Run(() => JsonConvert.DeserializeObject<PaginatedResponse<Activity>>(json));
+                foreach (Activity act in activities.Results)
+                {
+                    Activities.Add(act);
+                }
+            }
+        }
+
+        public async void ExecuteSelectActivity(Activity activity)
+        {
+            string message = $"Name: {activity.Name}\nOwner: {activity.Owner}\nDescription: {activity.Description}\nIs active? {activity.IsActive}\nLast time: {activity.LastTime}\n";
+
+            var more = await _pageDialog.DisplayAlertAsync("Contact details", message, "More", "Ok");
+            if (more)
+            {
+                string action = await _pageDialog.DisplayActionSheetAsync("What would you like to do?", "Cancel", null, "Edit", "Delete");
+
+                if (action == "Delete")
+                {
+                    int idx = Activities.IndexOf(activity);
+                    var res = await _apiManager.DeleteActivity(Config.Token, activity.Code);
+                    if (res.IsSuccessStatusCode)
+                    {
+                        var json = await res.Content.ReadAsStringAsync();
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        var json = await res.Content.ReadAsStringAsync();
+                        Console.WriteLine();
+                    }
+                    Activities.RemoveAt(idx);
+                }
+                else
+                {
+                    var navigationParams = new NavigationParameters();
+                    navigationParams.Add("activity", activity);
+                    await _navigationService.NavigateAsync(new Uri(NavigationConstants.EditActivityPage, UriKind.Relative), navigationParams);
+                }
+            }
+        }
+
+        private async void ExecuteAddActivity()
+        {
+            await _navigationService.NavigateAsync(new Uri(NavigationConstants.EditActivityPage, UriKind.Relative));
+        }
     }
 }
---
+++ Dockpad/ViewModels/EditActivityPageViewModel.cs
@@ -0,0 +1,76 @@
+﻿using Dockpad.Models;
+using Dockpad.Services;
+using Prism.Commands;
+using Prism.Navigation;
+using Prism.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Text;
+
+namespace Dockpad.ViewModels
+{
+    public class EditActivityPageViewModel : INavigationAware, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Activity Form { get; set; } = new Activity();
+
+        private IAPIManager _apiManager { get; set; }
+
+        private INavigationService _navigationService;
+
+        private IPageDialogService _pageDialog;
+
+        public DelegateCommand SaveCommand { get; set; }
+
+        private bool _is_new = false;
+
+        public EditActivityPageViewModel(INavigationService navigationService, IAPIManager apiManager, IPageDialogService pageDialog)
+        {
+            _apiManager = apiManager;
+            _navigationService = navigationService;
+            SaveCommand = new DelegateCommand(ExecuteSaveCommand);
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("activity"))
+            {
+                _is_new = true;
+                Activity activity = (Activity)parameters["activity"];
+                Form = activity;
+            }
+        }
+
+        public async void ExecuteSaveCommand()
+        {
+         
+            HttpResponseMessage response;
+
+            if (_is_new)
+            {
+                response = await _apiManager.PatchActivity(Config.Token, Form, Form.Code);
+            }
+            else
+            {
+                response = await _apiManager.PostActivity(Config.Token, Form);
+            }
+
+            if (response.IsSuccessStatusCode)
+            {
+                await _navigationService.GoBackAsync();
+            }
+            else
+            {
+                await _pageDialog.DisplayAlertAsync("An error ocurred", "An error ocurrer while trying to save the activity", "ok");
+            }
+        }
+    }
+}
